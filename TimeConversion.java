f14f44ebd9f3da552489b125661fa46becfe1ac1
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2023-12-26 16:48:42 +0530

    comment added

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,3 +1,8 @@
+// Given a time at 12-hour AM/PM format, convert it to military (24-hour) time.
+// Note: Midnight is 12:00:00 AM on a 12-hour clock and 00:00:00 on a 24-hour clock. 
+// Noon is 12:00:00 PM on 12-hour clock and 12:00:00 on 24-hour clock
+
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
